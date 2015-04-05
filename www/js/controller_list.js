e26198f7340b32976ef0885fd81e673ee7052af3
tobika <tobias.kausch@tobik.eu>
2015-04-05 18:41:04 +0200

    FIX delete back, no update in list, issue

--- www/js/controller_list.js
+++ www/js/controller_list.js
@@ -3,65 +3,35 @@ angular.module('starter.controllers').controller('ListCtrl', function($scope, Da
   $scope.allData = [];
 
   $scope.$on("$ionicView.beforeEnter", function( scopes, states ) {
-    if (Database.hasChanged('listController') === true) {
 
-      /*  $scope.noMoreItemsAvailable = false;
-          $scope.loadMore();*/
+    if (Database.hasChanged('listController') === true) {
 
       $scope.groups = ListService.getDaysList();
       Database.gotData('listController');
     }
   });
 
-  /*$scope.noMoreItemsAvailable = false;
-
-  $scope.loadMore = function() {
-    console.log("Load more");
-    
-    if ($scope.allData.length > 0) {
-      console.log("Increase, elements left: " + $scope.allData.length);
-      $timeout(function() {
-        var newElements = 5;
-        if ($scope.allData.length < 5) {
-          newElements = $scope.allData.length;
-        }
-        console.log("New elements: " + newElements);
-        var tmp = $scope.allData.splice(0, newElements);
-        for (var i = 0; i < 5; i++) {
-          // check if element is undefined, happens because of strange splice error
-          if (tmp[i]) {
-            $scope.listData.push(tmp[i]);
-          }
-        }
-        $scope.$broadcast('scroll.infiniteScrollComplete');
-      });
-    }    
-    else {
-      $timeout(function() {
-        console.log("Infinite Scroll End");
-        $scope.noMoreItemsAvailable = true;
-      });  
-    }  
-  };*/
-
 })
 
 .controller('ListDetailDayCtrl', function($scope, $stateParams, ListService) {
 
   $scope.$on("$ionicView.beforeEnter", function( scopes, states ) {
-    $scope.listData = ListService.getDayList($stateParams.dayId);
-    console.log(JSON.stringify($scope.listData));
+    ListService.initListService().then(function() {
+      $scope.listData = ListService.getDayList($stateParams.dayId);
+      console.log(JSON.stringify($scope.listData));
+    });
   });
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Database, $ionicNavBarDelegate, $state) {
+.controller('FriendDetailCtrl', function($scope, $stateParams, Database, $ionicHistory) {
 
   $scope.$on("$ionicView.beforeEnter", function( scopes, states ) {
     $scope.element = Database.get($stateParams.friendId);
   });
 
   $scope.deleteElement = function() {
+
   	Database.deleteElement($scope.element.id);
-    $state.go('tab.list');
+    $ionicHistory.goBack();
   };
 });
